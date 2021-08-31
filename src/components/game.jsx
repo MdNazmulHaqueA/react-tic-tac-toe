a09c09ca88a4d01ed41b98d15da5a22862c80bec
MdNazmulHaqueA <naz089fvrict@gmail.com>
2021-09-01 05:07:22 +0600

    board and game component basic setup

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-
+import Board from './board';
 
 class Game extends React.Component {
-	
+    state = {
+        history: [{ squares: Array(9).fill(null) }]
+    };
+    onClick = i => {
+        console.log(i);
+    };
 
-	render() {
-		
-		return (
-			<div>
-				<h1>The game component</h1>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div>
+                <h1>The game component</h1>
+                <Board onClick={this.onClick} squares={this.state.history[0]} />
+            </div>
+        );
+    }
 }
 
 export default Game;
