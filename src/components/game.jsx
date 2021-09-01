1dbd0e67215801da065a3dac1b37aed9f94d2b34
MdNazmulHaqueA <naz089fvrict@gmail.com>
2021-09-01 21:25:38 +0600

    game ready

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import Board from './board';
 function calculateWinner(squares) {
-    return;
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 class Game extends React.Component {
     state = {
@@ -14,7 +31,7 @@ class Game extends React.Component {
         const current = history[history.length - 1];
         const squares = current.squares.slice();
 
-        if (calculateWinner || squares[i]) {
+        if (calculateWinner(squares) || squares[i]) {
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -26,10 +43,14 @@ class Game extends React.Component {
     };
 
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+        console.log(winner);
         return (
             <div>
                 <h1>The game component</h1>
-                <Board onClick={this.handleClick} squares={this.state.history[0]} />
+                <Board onClick={this.handleClick} squares={current.squares} />
             </div>
         );
     }
